bb2dc98d8f96d433538e15dc2979e3dfc0f7cbd7
Marko Lukša <marko.luksa@gmail.com>
2019-05-03 16:13:01 +0200

    Ensure Pilot can run without privileges for reading nodes
    By running Pilot with --podLocalitySource=pod, it reads the pod's labels
    to determine pod locality instead of obtaining that information from the
    pod's node. An additional controller with cluster-level privileges is
    required to copy those labels from the node to the pod after the pod is
    scheduled.

--- pilot/cmd/pilot-discovery/main.go
+++ pilot/cmd/pilot-discovery/main.go
@@ -129,6 +129,8 @@ func init() {
 		"Directory to watch for updates to config yaml files. If specified, the files will be used as the source of config, rather than a CRD client.")
 	discoveryCmd.PersistentFlags().StringVarP(&serverArgs.Config.ControllerOptions.WatchedNamespaces, "appNamespace", "a",
 		metav1.NamespaceAll, "Specify the applications namespace list the controller manages, separated by comma; if not set, controller watches all namespaces")
+	discoveryCmd.PersistentFlags().StringVarP(&serverArgs.Config.ControllerOptions.PodLocalitySource, "podLocalitySource", "",
+		"node", "Specify where the controller should obtain the Pod's zone and region from (the pod's node or the pod itself)")
 	discoveryCmd.PersistentFlags().DurationVar(&serverArgs.Config.ControllerOptions.ResyncPeriod, "resync", 60*time.Second,
 		"Controller resync interval")
 	discoveryCmd.PersistentFlags().StringVar(&serverArgs.Config.ControllerOptions.DomainSuffix, "domain", "cluster.local",
