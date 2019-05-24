b629cd2f0fff67757283b0253d17c958bfbc1bcf
Kevin Conner <kconner@redhat.com>
2019-05-24 06:29:24 -0700

    MAISTRA-439 Override the watched namespaces for multi tenancy

--- galley/pkg/server/settings/args.go
+++ galley/pkg/server/settings/args.go
@@ -45,13 +45,16 @@ type Args struct { // nolint:maligned
 	// The path to kube configuration file.
 	KubeConfig string
 
+	// deprecated
+	AppNamespace string
+
 	// Namespace list the controller watches, separated by comma; if not set, controller watches all namespaces"
 	WatchedNamespaces string
 
 	// The name of the MemberRoll resource.  If specified the server will monitor this resource to discover the application namespaces.
 	MemberRollName string
 	// The namespace for the MemberRoll resource, this is the same as the validation deployment namespace
-	MemberRollNameNamespace string
+	MemberRollNamespace string
 
 	// resync period to be passed to the K8s machinery.
 	ResyncPeriod time.Duration
@@ -193,6 +196,7 @@ func (a *Args) String() string {
 	_, _ = fmt.Fprintf(buf, "WatchedNamespaces: %v\n", a.WatchedNamespaces)
 	_, _ = fmt.Fprintf(buf, "ResyncPeriod: %v\n", a.ResyncPeriod)
 	_, _ = fmt.Fprintf(buf, "MemberRollName: %v\n", a.MemberRollName)
+	_, _ = fmt.Fprintf(buf, "MemberRollNamespace: %v\n", a.MemberRollNamespace)
 	_, _ = fmt.Fprintf(buf, "APIAddress: %s\n", a.APIAddress)
 	_, _ = fmt.Fprintf(buf, "EnableGrpcTracing: %v\n", a.EnableGRPCTracing)
 	_, _ = fmt.Fprintf(buf, "MaxReceivedMessageSize: %d\n", a.MaxReceivedMessageSize)
