1e67b9bc4e2db35192a38fa0f2afb44870148a3b
Daniel Grimm <dgrimm@redhat.com>
2019-09-02 14:46:50 +0200

    MAISTRA-862 Prevent deadlock in case of Watch() errors in subsequent List()

--- pkg/listwatch/listwatch.go
+++ pkg/listwatch/listwatch.go
@@ -134,7 +134,7 @@ func (mlw *multiListerWatcher) List(options metav1.ListOptions) (runtime.Object,
 	defer mlw.lock.Unlock()
 
 	if mlw.state == watching || mlw.state == stopping {
-		mlw.Stop()
+		mlw.stopInternal()
 	}
 
 	mlw.lwMap = make(map[string]*listerWatcher)
@@ -309,7 +309,11 @@ func (mlw *multiListerWatcher) ResultChan() <-chan watch.Event {
 func (mlw *multiListerWatcher) Stop() {
 	mlw.lock.Lock()
 	defer mlw.lock.Unlock()
+	mlw.stopInternal()
+}
 
+// This must be called with lock held.
+func (mlw *multiListerWatcher) stopInternal() {
 	select {
 	case <-mlw.stopped:
 		// nothing to do, we are already stopped
