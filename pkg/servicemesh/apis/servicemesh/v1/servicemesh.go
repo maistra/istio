33a0d8c95697b348c0ccb304ca6159706f67441b
Daniel Grimm <dgrimm@redhat.com>
2019-12-18 15:37:24 +0100

    Add MemberRollController
    Squashed commit, consisting of:
    
      * MAISTRA-417: Add MemberRollController
    
      * MAISTRA-450: Switch across to ConfiguredMembers and check existing
        config for changes
    
      * MAISTRA-1005 Do not expect namespaces to have a stable ordering
    
    The clients have also been regenerated.

---
+++ pkg/servicemesh/apis/servicemesh/v1/servicemesh.go
@@ -0,0 +1,39 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ServiceMeshMemberRoll is the Schema for the servicemeshmemberrolls API
+// +k8s:openapi-gen=true
+type ServiceMeshMemberRoll struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ServiceMeshMemberRollSpec   `json:"spec,omitempty"`
+	Status ServiceMeshMemberRollStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ServiceMeshMemberRollList contains a list of ServiceMeshMemberRoll
+type ServiceMeshMemberRollList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ServiceMeshMemberRoll `json:"items"`
+}
+
+// ServiceMeshMemberRollSpec defines the members of the mesh
+type ServiceMeshMemberRollSpec struct {
+	Members []string `json:"members,omitempty"`
+}
+
+// ServiceMeshMemberRollStatus contains the state last used to reconcile the list
+type ServiceMeshMemberRollStatus struct {
+	ObservedGeneration    int64    `json:"observedGeneration,omitempty"`
+	ServiceMeshGeneration int64    `json:"meshGeneration,omitempty"`
+	ConfiguredMembers     []string `json:"configuredMembers,omitempty"`
+}
