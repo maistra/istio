3e318dcf73ca160820b5b1ea8deaeb84391fd7cc
Daniel Grimm <dgrimm@redhat.com>
2020-05-26 13:48:15 +0200

    MAISTRA-1449 Update to 1.4.8 (#110)
    * authz: fix the validation for request.headers (#21284)
    
    * manual backport of 21513 (#21514)
    
    Signed-off-by: Rama Chavali <rama.rao@salesforce.com>
    
    * Fix PSP sample file to allow NET_RAW. (#21533)
    
    * [release-1.4] remove use_downstream_protocol for gateway (#21727)
    
    * remove use_downstream_protocol for gateway
    
    * comment
    
    * writing
    
    Co-authored-by: Yan Xue <3491507+yxue@users.noreply.github.com>
    
    * Update dependencies (#21765)
    
    * Make iptables script and output consistent with golang (#21871)
    
    Co-authored-by: knrc <knrc@users.noreply.github.com>
    
    * [release-1.4] resign certificate (#22131)
    
    * resign certificate
    
    * test
    
    Co-authored-by: xuzhonghu <xuzhonghu@huawei.com>
    
    * Stop linting links of 1.4 branch (#22132)
    
    This is going to continue to break as istio.io changes. We already
    disabled this on 1.5/master.
    
    * [release-1.4] Build: Honor GOBUILDFLAGS variable also in test mode (#22171)
    
    We already honor it in `go build`, we should do the same
    in `go test`.
    
    Manual backport of https://github.com/istio/istio/pull/22163
    
    * Update base image for release 1.4 (#22165)
    
    * Fix GOBUILDFLAGS usage in codecov scripts (#22182)
    
    This variable needs to be set before used (set -u) and must not be
    quoted, otherwise it will become an empty arg for go test if it's empty.
    
    Co-authored-by: Jonh Wendell <jonh.wendell@redhat.com>
    
    * Update proxy to pickup fixes for ISTIO-SECURITY-2020-003. (#22257)
    
    * Fix extra .Value in deployment file for stackdriver tracing vars (#22506)
    
    Co-authored-by: gargnupur <gargnupur@google.com>
    
    * Update operator SHA (#22523)
    
    * clone LbEndpoint to prevent data race (#22023) (#22528)
    
    (cherry picked from commit fdc6dd4143680af197dbab8f5f5afdf8fb6c1c06)
    
    Co-authored-by: Zhonghu Xu <xuzhonghu@huawei.com>
    
    * cni: update SHA (#22569)
    
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>
    
    * Make sure to use CNI 1.4 image when running tests under CNI (#23035)
    
    * Make sure to use CNI 1.4 image when running tests under CNI
    
    * Fix lint
    
    * [release-1.4] Update dependencies with update_deps.sh (#23010)
    
    * Update deps with update_deps.sh
    
    * Dep updates again
    
    * [release-1.4] Run update_deps.sh (#23051)
    
    * Run update_deps.sh
    
    Had to do go get istio.io/operator@5f8ecc70a0f4059bfd4d0f7867d4dc76407f2f08
    since update_deps.sh was not updating it.
    
    * Run go mod tidy
    
    * Update with another cni change (#23061)
    
    * [release-1.4] Fix Gateway helm chart for helm 3 (#23077)
    
    * Fix Gateway helm chart for helm 3
    
    Helm 3 threw an error that `$spec.sds` is not a function. Removing the parentheses led to the next problem: the and evaluates eager so `spec.sds.enabled` resulted in a `nil` exception.
    
    By nesting the if, the problem is resolved
    
    * Also fix the role
    
    Co-authored-by: Alex Nederlof <alex@nederlof.com>
    
    Co-authored-by: Yangmin Zhu <ymzhu@google.com>
    Co-authored-by: Rama Chavali <rama.rao@salesforce.com>
    Co-authored-by: Oliver Liu <yonggangl@google.com>
    Co-authored-by: Istio Automation <istio.testing@gmail.com>
    Co-authored-by: Yan Xue <3491507+yxue@users.noreply.github.com>
    Co-authored-by: Jonh Wendell <jonh.wendell@redhat.com>
    Co-authored-by: knrc <knrc@users.noreply.github.com>
    Co-authored-by: xuzhonghu <xuzhonghu@huawei.com>
    Co-authored-by: John Howard <howardjohn@google.com>
    Co-authored-by: Eric Van Norman <ericvn@us.ibm.com>
    Co-authored-by: Joshua Blatt <jblatt@google.com>
    Co-authored-by: gargnupur <gargnupur@google.com>
    Co-authored-by: Yuchen Dai <silentdai@gmail.com>
    Co-authored-by: jacob-delgado <38300436+jacob-delgado@users.noreply.github.com>
    Co-authored-by: Alex Nederlof <alex@nederlof.com>

--- prow/release-commit.sh
+++ prow/release-commit.sh
@@ -37,7 +37,7 @@ DOCKER_HUB=${DOCKER_HUB:-gcr.io/istio-testing}
 GCS_BUCKET=${GCS_BUCKET:-istio-build/dev}
 
 # Use a pinned version in case breaking changes are needed
-BUILDER_SHA=898c5ac1c2fd507a95016b55026a21dd2a002432
+BUILDER_SHA=226efab93b38fe2573eccecdedcbaa5cea51a3be
 
 # Reference to the next minor version of Istio
 # This will create a version like 1.4-alpha.sha
