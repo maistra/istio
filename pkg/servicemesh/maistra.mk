ac37f9999f6b8c10dbb25aab4b5e097ddc2de1b0
Brad Ison <brad.ison@redhat.com>
2021-02-10 17:19:22 +0100

    MAISTRA-1801: Add ServiceMeshExtension types and clients
    Partial pick of 8b1ed09411 for Maistra 2.1 / Istio 1.8 rebase.
    
    This brings in the ServiceMeshExtension types and code-gen tasks, and
    adds the generated clients.  It moves the existing generated Maistra
    clients to the `v1` subdirectory of the client package.

--- pkg/servicemesh/maistra.mk
+++ pkg/servicemesh/maistra.mk
@@ -18,7 +18,8 @@ informer_gen = informer-gen
 
 kube_base_output_package = istio.io/istio/pkg/servicemesh
 kube_api_base_package = $(kube_base_output_package)/apis
-kube_api_packages = $(kube_api_base_package)/servicemesh/v1
+kube_api_packages_v1 = $(kube_api_base_package)/servicemesh/v1
+kube_api_packages_v1alpha1 = $(kube_api_base_package)/servicemesh/v1alpha1
 kube_clientset_package = $(kube_base_output_package)/clientset
 
 # file header text
@@ -26,11 +27,14 @@ kube_go_header_text = pkg/servicemesh/header.go.txt
 # clientset name used by kubernetes client-gen
 kube_clientset_name = versioned
 # base output package used by kubernetes client-gen
-kube_clientset_package = $(kube_base_output_package)/client/clientset
+kube_clientset_package_v1 = $(kube_base_output_package)/client/v1/clientset
+kube_clientset_package_v1alpha1 = $(kube_base_output_package)/client/v1alpha1/clientset
 # base output package used by kubernetes lister-gen
-kube_listers_package = $(kube_base_output_package)/client/listers
+kube_listers_package_v1 = $(kube_base_output_package)/client/v1/listers
+kube_listers_package_v1alpha1 = $(kube_base_output_package)/client/v1alpha1/listers
 # base output package used by kubernetes informer-gen
-kube_informers_package = $(kube_base_output_package)/client/informers
+kube_informers_package_v1 = $(kube_base_output_package)/client/v1/informers
+kube_informers_package_v1alpha1 = $(kube_base_output_package)/client/v1alpha1/informers
 
 ifeq ($(IN_BUILD_CONTAINER),1)
 	# k8s code generators rely on GOPATH, using $GOPATH/src as the base package
@@ -46,9 +50,12 @@ endif
 
 .PHONY: maistra-gen-k8s-client
 maistra-gen-k8s-client:
-	@$(client_gen) --clientset-name $(kube_clientset_name) --input-base "" --input  $(kube_api_packages) --output-package $(kube_clientset_package) -h $(kube_go_header_text)
-	@$(lister_gen) --input-dirs $(kube_api_packages) --output-package $(kube_listers_package) -h $(kube_go_header_text)
-	@$(informer_gen) --input-dirs $(kube_api_packages) --versioned-clientset-package $(kube_clientset_package)/$(kube_clientset_name) --listers-package $(kube_listers_package) --output-package $(kube_informers_package) -h $(kube_go_header_text)
+	@$(client_gen) --clientset-name $(kube_clientset_name) --input-base "" --input  $(kube_api_packages_v1) --output-package $(kube_clientset_package_v1) -h $(kube_go_header_text)
+	@$(lister_gen) --input-dirs $(kube_api_packages_v1) --output-package $(kube_listers_package_v1) -h $(kube_go_header_text)
+	@$(informer_gen) --input-dirs $(kube_api_packages_v1) --versioned-clientset-package $(kube_clientset_package_v1)/$(kube_clientset_name) --listers-package $(kube_listers_package_v1) --output-package $(kube_informers_package_v1) -h $(kube_go_header_text)
+	@$(client_gen) --clientset-name $(kube_clientset_name) --input-base "" --input  $(kube_api_packages_v1alpha1) --output-package $(kube_clientset_package_v1alpha1) -h $(kube_go_header_text)
+	@$(lister_gen) --input-dirs $(kube_api_packages_v1alpha1) --output-package $(kube_listers_package_v1alpha1) -h $(kube_go_header_text)
+	@$(informer_gen) --input-dirs $(kube_api_packages_v1alpha1) --versioned-clientset-package $(kube_clientset_package_v1alpha1)/$(kube_clientset_name) --listers-package $(kube_listers_package_v1alpha1) --output-package $(kube_informers_package_v1alpha1) -h $(kube_go_header_text)
 	@$(move_generated)
 
 .PHONY: vendor
@@ -57,5 +64,9 @@ vendor:
 	@go mod vendor
 	@echo "done updating vendor"
 
-.PHONY: gen
+.PHONY: maistra-gen
 maistra-gen: maistra-gen-k8s-client vendor
+
+.PHONY: mec
+mec: build
+BINARIES += ./mec/cmd/mec
