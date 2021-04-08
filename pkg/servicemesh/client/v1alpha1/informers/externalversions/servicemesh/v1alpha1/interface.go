03b772c1aef580867ebfdc76bf204e12faf13af0
rcernich <rcernich@redhat.com>
2021-04-08 16:24:43 +0200

    MAISTRA-2293 add CRD and controller for federating meshes
    - example istio config supporting service import/export across meshes
    
    Signed-off-by: rcernich <rcernich@redhat.com>

--- pkg/servicemesh/client/v1alpha1/informers/externalversions/servicemesh/v1alpha1/interface.go
+++ pkg/servicemesh/client/v1alpha1/informers/externalversions/servicemesh/v1alpha1/interface.go
@@ -22,6 +22,8 @@ import (
 
 // Interface provides access to all the informers in this group version.
 type Interface interface {
+	// MeshFederations returns a MeshFederationInformer.
+	MeshFederations() MeshFederationInformer
 	// ServiceMeshExtensions returns a ServiceMeshExtensionInformer.
 	ServiceMeshExtensions() ServiceMeshExtensionInformer
 }
@@ -35,6 +37,11 @@ type version struct {
 // New returns a new Interface.
 func New(f internalinterfaces.SharedInformerFactory, namespace string, tweakListOptions internalinterfaces.TweakListOptionsFunc) Interface {
 	return &version{factory: f, namespace: namespace, tweakListOptions: tweakListOptions}
+}
+
+// MeshFederations returns a MeshFederationInformer.
+func (v *version) MeshFederations() MeshFederationInformer {
+	return &meshFederationInformer{factory: v.factory, namespace: v.namespace, tweakListOptions: v.tweakListOptions}
 }
 
 // ServiceMeshExtensions returns a ServiceMeshExtensionInformer.
