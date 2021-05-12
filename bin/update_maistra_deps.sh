e575e85d0f2e7a36b3c8131121e6c7a786b2d5b5
Jonh Wendell <jonh.wendell@redhat.com>
2021-05-12 13:41:27 -0400

    [misc] MAISTRA-2346: Add script to update maistra deps (#337)
    It will be primarily invoked by a prow job. Currently it fetches updates
    from maistra/api only, but can be extended in the future.

---
+++ bin/update_maistra_deps.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Copyright Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eux
+
+UPDATE_BRANCH=${UPDATE_BRANCH:-"maistra-2.4"}
+
+# Update go dependencies
+go get -d "maistra.io/api@${UPDATE_BRANCH}"
+go mod tidy
+
+# Copy CRD's
+dir=$(mktemp -d)
+git clone "https://github.com/maistra/api.git" "${dir}"
+pushd "${dir}"
+git checkout "${UPDATE_BRANCH}"
+popd
+
+cp "${dir}"/manifests/federation.maistra.io_servicemeshpeers.yaml manifests/charts/base/crds
+cp "${dir}"/manifests/federation.maistra.io_exportedservicesets.yaml manifests/charts/base/crds
+cp "${dir}"/manifests/federation.maistra.io_importedservicesets.yaml manifests/charts/base/crds
+
+rm -rf "${dir}"
+
+# Regenerate files
+make clean gen
