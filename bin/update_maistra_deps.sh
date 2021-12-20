e963857bb9b999372e674c2f49b56a5f3cd2e477
Daniel Grimm <dgrimm@redhat.com>
2021-12-20 13:33:42 +0100

    [misc] OSSM-870 Fix ApplyServiceMeshCRDs function (#461)

--- bin/update_maistra_deps.sh
+++ bin/update_maistra_deps.sh
@@ -23,25 +23,16 @@ go get -d "maistra.io/api@${UPDATE_BRANCH}"
 go mod tidy
 go mod vendor
 
-# Copy CRD's
-dir=$(mktemp -d)
-git clone "https://github.com/maistra/api.git" "${dir}"
-pushd "${dir}"
-git checkout "${UPDATE_BRANCH}"
-popd
-
 # FIXME: https://issues.redhat.com/browse/MAISTRA-2353
 # For now we are just copying the files that already exist in istio, i.e., we are not adding any new files.
 # We should copy all CRD's from api repo, i.e., uncomment the lines below and delete the other copy commands
 # rm -f manifests/charts/base/crds/maistra*
 # cp "${dir}"/manifests/* manifests/charts/base/crds
 
-cp "${dir}"/manifests/federation.maistra.io_servicemeshpeers.yaml manifests/charts/base/crds
-cp "${dir}"/manifests/federation.maistra.io_exportedservicesets.yaml manifests/charts/base/crds
-cp "${dir}"/manifests/federation.maistra.io_importedservicesets.yaml manifests/charts/base/crds
-cp "${dir}"/manifests/maistra.io_servicemeshextensions.yaml manifests/charts/base/crds
-
-rm -rf "${dir}"
+cp ./vendor/maistra.io/api/manifests/federation.maistra.io_servicemeshpeers.yaml manifests/charts/base/crds
+cp ./vendor/maistra.io/api/manifests/federation.maistra.io_exportedservicesets.yaml manifests/charts/base/crds
+cp ./vendor/maistra.io/api/manifests/federation.maistra.io_importedservicesets.yaml manifests/charts/base/crds
+cp ./vendor/maistra.io/api/manifests/maistra.io_servicemeshextensions.yaml manifests/charts/base/crds
 
 # Regenerate files
 make clean gen
