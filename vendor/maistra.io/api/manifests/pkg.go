f470edd977d41ad0ed4f7e39432f4df6ad389976
Jacek Ewertowski <jacek.ewertowski1@gmail.com>
2022-01-27 15:18:34 +0100

    [misc] OSSM-796 add SMMR integration test (#458)

---
+++ vendor/maistra.io/api/manifests/pkg.go
@@ -0,0 +1,6 @@
+// DO NOT DELETE THIS FILE
+//
+// This file is needed because it allows other projects to
+// import the generated CRD manifests located in this folder
+// through the go module system.
+package manifests
