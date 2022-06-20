luksa/istio-maistra

[envoy] Do not download envoy-centos image (#234)
* Do not download envoy-centos image

We do not use it.

Also, change proxy SHA to match maistra proxy.

* Change proxy image to be centos based

And change default app sidecar to be centos based too.

Make `make docker` work again (#251)

- Point the Base URL to ours instead of upstream
- Download envoy-centos binary (Reverts
  https://github.com/maistra/istio/pull/234/commits/bb004bd823640ea15e747e21bbb7c0d706a6f9e3)
- Bump proxy SHA

pkg/test/framework/components/echo/config.go
@@ -49,7 +49,7 @@ const (
 	Centos7      VMDistro = "Centos7"
 	Centos8      VMDistro = "Centos8"
 
-	DefaultVMDistro = UbuntuBionic
+	DefaultVMDistro = Centos8
 )
 
 // Config defines the options for creating an Echo component.

maistra/istio

pkg/test/framework/components/echo/config.go
@@ -76,7 +76,7 @@ const (
 	Centos7      VMDistro = "Centos7"
 	Rockylinux8  VMDistro = "Centos8"
 
-	DefaultVMDistro = UbuntuJammy
+	DefaultVMDistro = Rockylinux8
 )
 
 // Config defines the options for creating an Echo component.