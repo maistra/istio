luksa/istio-maistra

[ior] MAISTRA-1400 Add IOR to Pilot
* [MAISTRA-1089][MAISTRA-1400][MAISTRA-1744][MAISTRA-1811]: Add IOR to Pilot (#135) (#240)

* MAISTRA-1400: Add IOR to Pilot (#135)

* MAISTRA-1400: Add IOR to Pilot

* [MAISTRA-1744] Add route annotation propagation (#158)

* MAISTRA-1811 Store resourceVersion of reconciled Gateway resource (#190)

* MAISTRA-1089 Add support for IOR routes in all namespaces (#193)

* MAISTRA-2131: ior: honor Gateway's httpsRedirect (#276)

If Gateway's httpsRedirect is set to true, create the OpenShift Route
with Insecure Policy set to `Redirect`.

Manual cherrypick from https://github.com/maistra/istio/pull/269.

* MAISTRA-2149: Make IOR robust in multiple replicas (#282)

In scenarios where multiple replicas of istiod are running,
only one IOR should be in charge of keeping routes in sync
with Istio Gateways. We achieve this by making sure IOR only
runs in the leader replica.

Also, because leader election is not 100% acurate, meaning
that for a small window of time there might be two instances
being the leader - which could lead to duplicated routes
being created if a new gateway is created in that time frame -
we also change the way the Route name is created: Instead of
having a generateName field, we now explicitly pass a name to
the Route object to be created. Being deterministic, it allows
the Route creation to fail when there's already a Route object
with the same name (created by the other leader in that time frame).

Use an exclusive leader ID for IOR

* Manual cherrypick of https://github.com/maistra/istio/pull/275

* MAISTRA-1813: Add unit tests for IOR (#286)

* MAISTRA-2051 fixes for maistra install

* MAISTRA-2164: Refactor IOR internals (#295)

Instead of doing lots of API calls on every event - this
does not scale well with lots of namespaces - keep the state
in memory, by doing an initial synchronization on start up and
updating it when receiving events.

The initial synchronization is more complex, as we have to deal with
async

pilot/pkg/config/kube/ior/ior.go
@@ -1,0 +1,91 @@
+// Copyright Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ior
+
+import (
+	"sync"
+
+	routev1 "github.com/openshift/client-go/route/clientset/versioned/typed/route/v1"
+
+	networking "istio.io/api/networking/v1alpha3"
+	"istio.io/istio/pilot/pkg/model"
+	"istio.io/istio/pkg/config"
+	"istio.io/istio/pkg/config/schema/collections"
+	"istio.io/istio/pkg/servicemesh/controller"
+	"istio.io/pkg/log"
+)
+
+// IORLog is IOR-scoped log
+var IORLog = log.RegisterScope("ior", "IOR logging", 0)
+
+// Register configures IOR component to respond to Gateway creations and removals
+func Register(
+	k8sClient KubeClient,
+	routerClient routev1.RouteV1Interface,
+	store model.ConfigStoreCache,
+	pilotNamespace string,
+	mrc controller.MemberRollController,
+	stop <-chan struct{},
+	errorChannel chan error) error {
+	IORLog.Info("Registering IOR component")
+
+	r, err := newRoute(k8sClient, routerClient, store, pilotNamespace, mrc, stop)
+	if err != nil {
+		return err
+	}
+
+	alive := true
+	var aliveLock sync.Mutex
+	go func(stop <-chan struct{}) {
+		// Stop responding to events when we are no longer a leader.
+		// Two notes here:
+		// (1) There's no such method "UnregisterEventHandler()"
+		// (2) It might take a few seconds to this channel to be closed. So, both pods might be leader for a few seconds.
+		<-stop
+		IORLog.Info("This pod is no longer a leader. IOR stopped responding")
+		aliveLock.Lock()
+		alive = false
+		aliveLock.Unlock()
+	}(stop)
+
+	IORLog.Debugf("Registering IOR into Istio's Gateway broadcast")
+	kind := collections.IstioNetworkingV1Alpha3Gateways.Resource().GroupVersionKind()
+	store.RegisterEventHandler(kind, func(_, curr config.Config, event model.Event) {
+		aliveLock.Lock()
+		defer aliveLock.Unlock()
+		if !alive {
+			return
+		}
+
+		// encapsulate in goroutine to not slow down processing because of waiting for mutex
+		go func() {
+			_, ok := curr.Spec.(*networking.Gateway)
+			if !ok {
+				IORLog.Errorf("could not decode object as Gateway. Object = %v", curr)
+				return
+			}
+
+			IORLog.Debugf("Event %v arrived. Object: %v", event, curr)
+			if err := r.handleEvent(event, curr); err != nil {
+				IORLog.Errora(err)
+				if errorChannel != nil {
+					errorChannel <- err
+				}
+			}
+		}()
+	})
+
+	return nil
+}

maistra/istio

pilot/pkg/config/kube/ior/ior.go
@@ -1,0 +1,92 @@
+// Copyright Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ior
+
+import (
+	"sync"
+
+	routev1 "github.com/openshift/client-go/route/clientset/versioned/typed/route/v1"
+
+	networking "istio.io/api/networking/v1alpha3"
+	"istio.io/istio/pilot/pkg/model"
+	"istio.io/istio/pkg/config"
+	"istio.io/istio/pkg/config/schema/collections"
+	"istio.io/istio/pkg/servicemesh/controller"
+	"istio.io/pkg/log"
+)
+
+// IORLog is IOR-scoped log
+var IORLog = log.RegisterScope("ior", "IOR logging", 0)
+
+// Register configures IOR component to respond to Gateway creations and removals
+func Register(
+	k8sClient KubeClient,
+	routerClient routev1.RouteV1Interface,
+	store model.ConfigStoreController,
+	pilotNamespace string,
+	mrc controller.MemberRollController,
+	stop <-chan struct{},
+	errorChannel chan error,
+) error {
+	IORLog.Info("Registering IOR component")
+
+	r, err := newRoute(k8sClient, routerClient, store, pilotNamespace, mrc, stop)
+	if err != nil {
+		return err
+	}
+
+	alive := true
+	var aliveLock sync.Mutex
+	go func(stop <-chan struct{}) {
+		// Stop responding to events when we are no longer a leader.
+		// Two notes here:
+		// (1) There's no such method "UnregisterEventHandler()"
+		// (2) It might take a few seconds to this channel to be closed. So, both pods might be leader for a few seconds.
+		<-stop
+		IORLog.Info("This pod is no longer a leader. IOR stopped responding")
+		aliveLock.Lock()
+		alive = false
+		aliveLock.Unlock()
+	}(stop)
+
+	IORLog.Debugf("Registering IOR into Istio's Gateway broadcast")
+	kind := collections.IstioNetworkingV1Alpha3Gateways.Resource().GroupVersionKind()
+	store.RegisterEventHandler(kind, func(_, curr config.Config, event model.Event) {
+		aliveLock.Lock()
+		defer aliveLock.Unlock()
+		if !alive {
+			return
+		}
+
+		// encapsulate in goroutine to not slow down processing because of waiting for mutex
+		go func() {
+			_, ok := curr.Spec.(*networking.Gateway)
+			if !ok {
+				IORLog.Errorf("could not decode object as Gateway. Object = %v", curr)
+				return
+			}
+
+			IORLog.Debugf("Event %v arrived. Object: %v", event, curr)
+			if err := r.handleEvent(event, curr); err != nil {
+				IORLog.Errora(err)
+				if errorChannel != nil {
+					errorChannel <- err
+				}
+			}
+		}()
+	})
+
+	return nil
+}