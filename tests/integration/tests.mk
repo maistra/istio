2520ad155d4e64b98bbfe985208079d1b8ca195c
Jacek Ewertowski <jewertow@redhat.com>
2022-07-25 18:15:25 +0200

    Run only federation tests
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

--- tests/integration/tests.mk
+++ tests/integration/tests.mk
@@ -103,7 +103,7 @@ test.integration.kube: test.integration.kube.presubmit
 # Presubmit integration tests targeting Kubernetes environment. Really used for postsubmit on different k8s versions.
 .PHONY: test.integration.kube.presubmit
 test.integration.kube.presubmit: | $(JUNIT_REPORT) check-go-tag
-	$(RUN_TEST) ./tests/integration/... -timeout 30m \
+	$(RUN_TEST) ./tests/integration/servicemesh/federation/... -timeout 30m \
 	${_INTEGRATION_TEST_FLAGS} ${_INTEGRATION_TEST_SELECT_FLAGS} \
 	2>&1 | tee >($(JUNIT_REPORT) > $(JUNIT_OUT))
 
