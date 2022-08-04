luksa/istio-maistra

[federation] MAISTRA-2309 create CRD for FederationStatus (#348)
Signed-off-by: rcernich <rcernich@redhat.com>

pilot/pkg/serviceregistry/federation/controller.go
@@ -42,6 +42,7 @@ import (
 	"istio.io/istio/pkg/network"
 	"istio.io/istio/pkg/servicemesh/federation/common"
 	federationmodel "istio.io/istio/pkg/servicemesh/federation/model"
+	"istio.io/istio/pkg/servicemesh/federation/status"
 	"istio.io/pkg/log"
 )
 
@@ -72,9 +73,10 @@ type Controller struct {
 
 	logger *log.Scope
 
-	kubeClient  kube.Client
-	configStore model.ConfigStoreCache
-	xdsUpdater  model.XDSUpdater
+	kubeClient    kube.Client
+	statusHandler status.Handler
+	configStore   model.ConfigStoreCache
+	xdsUpdater    model.XDSUpdater
 
 	localDomainSuffix   string
 	defaultDomainSuffix string
@@ -110,6 +112,7 @@ type Options struct {
 	Network        string
 	Namespace      string
 	KubeClient     kube.Client
+	StatusHandler  status.Handler
 	ConfigStore    model.ConfigStoreCache
 	XDSUpdater     model.XDSUpdater
 	ResyncPeriod   time.Duration
@@ -152,6 +155,7 @@ func NewController(opt Options, mesh *v1alpha1.MeshFederation, defaultImportConf
 		serviceStore:        map[host.Name]*model.Service{},
 		instanceStore:       map[host.Name][]*model.ServiceInstance{},
 		kubeClient:          opt.KubeClient,
+		statusHandler:       opt.StatusHandler,
 		configStore:         opt.ConfigStore,
 		xdsUpdater:          opt.XDSUpdater,
 		resyncPeriod:        opt.ResyncPeriod,
@@ -423,6 +427,7 @@ func (c *Controller) convertServices(serviceList *federationmodel.ServiceListMes
 	for key, existing := range oldImports {
 		if _, exists := c.imports[key]; !exists {
 			updatedConfigs := c.deleteService(&federationmodel.ServiceMessage{ServiceKey: key}, existing)
+			c.statusHandler.ImportRemoved(key.Hostname)
 			for key, value := range updatedConfigs {
 				allUpdatedConfigs[key] = value
 			}
@@ -576,6 +581,8 @@ func (c *Controller) handleEvent(e *federationmodel.WatchEvent) {
 		return
 	}
 
+	c.statusHandler.WatchEventReceived()
+
 	unlockIt := true
 	c.storeLock.Lock()
 	defer func() {
@@ -635,6 +642,7 @@ func (c *Controller) handleEvent(e *federationmodel.WatchEvent) {
 		}
 	case federationmodel.ActionDelete:
 		updatedConfigs = c.deleteService(e.Service, existing)
+		c.statusHandler.ImportRemoved(e.Service.Hostname)
 	}
 	if len(updatedConfigs) > 0 {
 		c.logger.Debugf("pushing XDS config for services: %+v", updatedConfigs)
@@ -664,6 +672,7 @@ func (c *Controller) addService(service *federationmodel.ServiceMessage) (map[mo
 		Attributes: model.ServiceAttributes{Name: service.Name, Namespace: service.Namespace},
 	})
 	if importedName == nil {
+		c.statusHandler.ImportAdded(federationmodel.ServiceKey{}, service.Hostname)
 		c.logger.Debugf("skipping import of service %+v, as it does not match an import filter", service.ServiceKey)
 		return nil, nil
 	}
@@ -693,6 +702,8 @@ func (c *Controller) addService(service *federationmodel.ServiceMessage) (map[mo
 		Namespace: localService.Attributes.Namespace,
 	}] = struct{}{}
 
+	c.statusHandler.ImportAdded(*importedName, service.Hostname)
+
 	return updatedConfigs, nil
 }
 
@@ -706,6 +717,7 @@ func (c *Controller) updateService(service *federationmodel.ServiceMessage, exis
 		Attributes: model.ServiceAttributes{Name: service.Name, Namespace: service.Namespace},
 	})
 	if importedName == nil {
+		c.statusHandler.ImportUpdated(federationmodel.ServiceKey{}, service.Hostname)
 		if existing != nil {
 			c.logger.Debugf("deleting import for service %+v, as it no longer matches an import filter", service.ServiceKey)
 			return c.deleteService(service, existing), nil
@@ -758,6 +770,8 @@ func (c *Controller) updateService(service *federationmodel.ServiceMessage, exis
 		Name:      string(localService.Hostname),
 		Namespace: localService.Attributes.Namespace,
 	}] = struct{}{}
+
+	c.statusHandler.ImportUpdated(*importedName, service.Hostname)
 
 	return updatedConfigs, nil
 }
@@ -834,6 +848,7 @@ func (c *Controller) removeServiceFromStore(service federationmodel.ServiceKey) 
 }
 
 func (c *Controller) watch(eventCh chan *federationmodel.WatchEvent, stopCh <-chan struct{}) error {
+	c.statusHandler.WatchInitiated()
 	url := c.discoveryURL + "/watch"
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
@@ -842,6 +857,15 @@ func (c *Controller) watch(eventCh chan *federationmodel.WatchEvent, stopCh <-ch
 	}
 	req.Header.Add("discovery-address", c.networkAddress)
 	resp, err := http.DefaultClient.Do(req)
+	defer func() {
+		status := ""
+		if resp != nil {
+			status = resp.Status
+		} else if err != nil {
+			status = err.Error()
+		}
+		c.statusHandler.WatchTerminated(status)
+	}()
 	if err != nil {
 		return err
 	}
@@ -849,10 +873,13 @@ func (c *Controller) watch(eventCh chan *federationmodel.WatchEvent, stopCh <-ch
 		return fmt.Errorf("status code is not OK: %v (%s)", resp.StatusCode, resp.Status)
 	}
 
+	c.statusHandler.Watching()
+
 	// connection was established successfully. reset backoffPolicy
 	c.backoffPolicy.Reset()
 
 	dec := json.NewDecoder(resp.Body)
+	defer resp.Body.Close()
 	for {
 		select {
 		case <-stopCh:
@@ -876,6 +903,7 @@ func (c *Controller) resync() uint64 {
 	c.updateGateways(svcList)
 	if svcList != nil {
 		c.convertServices(svcList)
+		c.statusHandler.FullSyncComplete()
 		return svcList.Checksum
 	}
 	return 0

pkg/servicemesh/federation/server/server.go
@@ -34,6 +34,7 @@ import (
 	"istio.io/istio/pkg/config/host"
 	"istio.io/istio/pkg/servicemesh/federation/common"
 	federationmodel "istio.io/istio/pkg/servicemesh/federation/model"
+	"istio.io/istio/pkg/servicemesh/federation/status"
 	"istio.io/pkg/log"
 )
 
@@ -49,7 +50,7 @@ type Options struct {
 }
 
 type FederationManager interface {
-	AddMeshFederation(mesh *v1alpha1.MeshFederation, exports *v1alpha1.ServiceExports) error
+	AddMeshFederation(mesh *v1alpha1.MeshFederation, exports *v1alpha1.ServiceExports, statusHandler status.Handler) error
 	DeleteMeshFederation(name string)
 	UpdateExportsForMesh(exports *v1alpha1.ServiceExports) error
 	DeleteExportsForMesh(name string)
@@ -120,7 +121,7 @@ func (s *Server) ingressServiceName(mesh *v1alpha1.MeshFederation) string {
 	return fmt.Sprintf("%s.%s.svc.%s", mesh.Spec.Gateways.Ingress.Name, mesh.Namespace, s.env.DomainSuffix)
 }
 
-func (s *Server) AddMeshFederation(mesh *v1alpha1.MeshFederation, exports *v1alpha1.ServiceExports) error {
+func (s *Server) AddMeshFederation(mesh *v1alpha1.MeshFederation, exports *v1alpha1.ServiceExports, statusHandler status.Handler) error {
 	exportConfig := common.NewServiceExporter(exports, s.defaultExportConfig, exportDomainSuffix(mesh.Name))
 
 	untypedMeshServer, ok := s.meshes.Load(mesh.Name)
@@ -133,6 +134,7 @@ func (s *Server) AddMeshFederation(mesh *v1alpha1.MeshFederation, exports *v1alp
 		env:                      s.env,
 		mesh:                     mesh,
 		exportConfig:             exportConfig,
+		statusHandler:            statusHandler,
 		configStore:              s.configStore,
 		ingressService:           s.ingressServiceName(mesh),
 		currentServices:          make(map[federationmodel.ServiceKey]*federationmodel.ServiceMessage),
@@ -205,6 +207,8 @@ func (s *Server) handleServiceList(response http.ResponseWriter, request *http.R
 		response.WriteHeader(500)
 		return
 	}
+	connection := getClientConnectionKey(request)
+	mesh.statusHandler.FullSyncSent(connection)
 }
 
 func (s *Server) handleWatch(response http.ResponseWriter, request *http.Request) {
@@ -214,7 +218,7 @@ func (s *Server) handleWatch(response http.ResponseWriter, request *http.Request
 		response.WriteHeader(400)
 		return
 	}
-	mesh.handleWatch(response)
+	mesh.handleWatch(response, request)
 }
 
 func (s *Server) Run(stopCh <-chan struct{}) {
@@ -316,7 +320,8 @@ type meshServer struct {
 	mesh         *v1alpha1.MeshFederation
 	exportConfig *common.ServiceExporter
 
-	configStore model.ConfigStoreCache
+	statusHandler status.Handler
+	configStore   model.ConfigStoreCache
 
 	ingressService  string
 	gatewaySAs      []string
@@ -401,12 +406,23 @@ func (s *meshServer) handleServiceList(response http.ResponseWriter) {
 	}
 }
 
-func (s *meshServer) handleWatch(response http.ResponseWriter) {
+func getClientConnectionKey(request *http.Request) string {
+	forwardedIPs := strings.Split(request.Header.Get("X-Forwarded-For"), ",")
+	if len(forwardedIPs) > 0 {
+		return strings.TrimSpace(forwardedIPs[0])
+	}
+	return request.RemoteAddr
+}
+
+func (s *meshServer) handleWatch(response http.ResponseWriter, request *http.Request) {
 	watch := make(chan *federationmodel.WatchEvent)
 	s.watchMut.Lock()
 	s.currentWatches = append(s.currentWatches, watch)
 	s.watchMut.Unlock()
+	connection := getClientConnectionKey(request)
+	s.statusHandler.RemoteWatchAccepted(connection)
 	defer func() {
+		s.statusHandler.RemoteWatchTerminated(connection)
 		s.watchMut.Lock()
 		for i, w := range s.currentWatches {
 			if w == watch {
@@ -426,7 +442,12 @@ func (s *meshServer) handleWatch(response http.ResponseWriter) {
 	}
 	flusher.Flush()
 	for {
-		event := <-watch
+		var event *federationmodel.WatchEvent
+		select {
+		case event = <-watch:
+		case <-request.Context().Done():
+			return
+		}
 		respBytes, err := json.Marshal(event)
 		if err != nil {
 			s.logger.Errorf("error marshaling watch event: %s", err)
@@ -443,6 +464,7 @@ func (s *meshServer) handleWatch(response http.ResponseWriter) {
 			return
 		}
 		flusher.Flush()
+		s.statusHandler.WatchEventSent(connection)
 	}
 }
 
@@ -485,6 +507,9 @@ func (s *meshServer) resync() {
 			s.logger.Debugf("exporting service %+v as %+v", svcKey, svcMessage.ServiceKey)
 			s.addService(svcKey, svcMessage)
 		}
+	}
+	if err := s.statusHandler.Flush(); err != nil {
+		s.logger.Errorf("error updating federation export status for mesh %s: %s", s.mesh.Name, err)
 	}
 }
 
@@ -587,6 +612,7 @@ func (s *meshServer) addService(svc federationmodel.ServiceKey, msg *federationm
 		Action:  federationmodel.ActionAdd,
 		Service: msg,
 	}
+	s.statusHandler.ExportAdded(svc, msg.Hostname)
 	s.pushWatchEvent(e)
 }
 
@@ -598,6 +624,7 @@ func (s *meshServer) updateService(svc federationmodel.ServiceKey, msg *federati
 		Action:  federationmodel.ActionUpdate,
 		Service: msg,
 	}
+	s.statusHandler.ExportUpdated(svc, msg.Hostname)
 	s.pushWatchEvent(e)
 }
 
@@ -612,6 +639,7 @@ func (s *meshServer) deleteService(svc federationmodel.ServiceKey, msg *federati
 		Action:  federationmodel.ActionDelete,
 		Service: msg,
 	}
+	s.statusHandler.ExportRemoved(svc)
 	s.pushWatchEvent(e)
 }
 

maistra/istio

pilot/pkg/serviceregistry/federation/controller.go
@@ -42,6 +42,7 @@ import (
 	"istio.io/istio/pkg/network"
 	"istio.io/istio/pkg/servicemesh/federation/common"
 	federationmodel "istio.io/istio/pkg/servicemesh/federation/model"
+	"istio.io/istio/pkg/servicemesh/federation/status"
 	"istio.io/pkg/log"
 )
 
@@ -72,9 +73,10 @@ type Controller struct {
 
 	logger *log.Scope
 
-	kubeClient  kube.Client
-	configStore model.ConfigStoreController
-	xdsUpdater  model.XDSUpdater
+	kubeClient    kube.Client
+	statusHandler status.Handler
+	configStore   model.ConfigStoreController
+	xdsUpdater    model.XDSUpdater
 
 	localDomainSuffix   string
 	defaultDomainSuffix string
@@ -110,6 +112,7 @@ type Options struct {
 	Network        string
 	Namespace      string
 	KubeClient     kube.Client
+	StatusHandler  status.Handler
 	ConfigStore    model.ConfigStore
 	XDSUpdater     model.XDSUpdater
 	ResyncPeriod   time.Duration
@@ -152,6 +155,7 @@ func NewController(opt Options, mesh *v1alpha1.MeshFederation, defaultImportConf
 		serviceStore:        map[host.Name]*model.Service{},
 		instanceStore:       map[host.Name][]*model.ServiceInstance{},
 		kubeClient:          opt.KubeClient,
+		statusHandler:       opt.StatusHandler,
 		configStore:         opt.ConfigStore,
 		xdsUpdater:          opt.XDSUpdater,
 		resyncPeriod:        opt.ResyncPeriod,
@@ -235,7 +239,8 @@ func (c *Controller) convertExportedService(s *federationmodel.ServiceMessage) (
 }
 
 func (c *Controller) convertToLocalService(s *federationmodel.ServiceMessage,
-	importedName federationmodel.ServiceKey) (*model.Service, []*model.ServiceInstance) {
+	importedName federationmodel.ServiceKey,
+) (*model.Service, []*model.ServiceInstance) {
 	serviceName := fmt.Sprintf("%s.%s.%s.local", importedName.Name, importedName.Namespace, c.clusterID)
 	serviceNamespace := importedName.Namespace
 	if !strings.HasSuffix(importedName.Hostname, c.localDomainSuffix) {
@@ -249,7 +254,8 @@ func (c *Controller) convertToLocalService(s *federationmodel.ServiceMessage,
 }
 
 func (c *Controller) createService(s *federationmodel.ServiceMessage, serviceName, serviceNamespace, hostname string, clusterID cluster.ID, network network.ID,
-	serviceVisibility visibility.Instance, networkGateways []model.NetworkGateway, sas []string) (*model.Service, []*model.ServiceInstance) {
+	serviceVisibility visibility.Instance, networkGateways []model.NetworkGateway, sas []string,
+) (*model.Service, []*model.ServiceInstance) {
 	instances := []*model.ServiceInstance{}
 	svc := &model.Service{
 		Attributes: model.ServiceAttributes{
@@ -423,6 +429,7 @@ func (c *Controller) convertServices(serviceList *federationmodel.ServiceListMes
 	for key, existing := range oldImports {
 		if _, exists := c.imports[key]; !exists {
 			updatedConfigs := c.deleteService(&federationmodel.ServiceMessage{ServiceKey: key}, existing)
+			c.statusHandler.ImportRemoved(key.Hostname)
 			for key, value := range updatedConfigs {
 				allUpdatedConfigs[key] = value
 			}
@@ -576,6 +583,8 @@ func (c *Controller) handleEvent(e *federationmodel.WatchEvent) {
 		return
 	}
 
+	c.statusHandler.WatchEventReceived()
+
 	unlockIt := true
 	c.storeLock.Lock()
 	defer func() {
@@ -635,6 +644,7 @@ func (c *Controller) handleEvent(e *federationmodel.WatchEvent) {
 		}
 	case federationmodel.ActionDelete:
 		updatedConfigs = c.deleteService(e.Service, existing)
+		c.statusHandler.ImportRemoved(e.Service.Hostname)
 	}
 	if len(updatedConfigs) > 0 {
 		c.logger.Debugf("pushing XDS config for services: %+v", updatedConfigs)
@@ -664,6 +674,7 @@ func (c *Controller) addService(service *federationmodel.ServiceMessage) (map[mo
 		Attributes: model.ServiceAttributes{Name: service.Name, Namespace: service.Namespace},
 	})
 	if importedName == nil {
+		c.statusHandler.ImportAdded(federationmodel.ServiceKey{}, service.Hostname)
 		c.logger.Debugf("skipping import of service %+v, as it does not match an import filter", service.ServiceKey)
 		return nil, nil
 	}
@@ -693,6 +704,8 @@ func (c *Controller) addService(service *federationmodel.ServiceMessage) (map[mo
 		Namespace: localService.Attributes.Namespace,
 	}] = struct{}{}
 
+	c.statusHandler.ImportAdded(*importedName, service.Hostname)
+
 	return updatedConfigs, nil
 }
 
@@ -706,6 +719,7 @@ func (c *Controller) updateService(service *federationmodel.ServiceMessage, exis
 		Attributes: model.ServiceAttributes{Name: service.Name, Namespace: service.Namespace},
 	})
 	if importedName == nil {
+		c.statusHandler.ImportUpdated(federationmodel.ServiceKey{}, service.Hostname)
 		if existing != nil {
 			c.logger.Debugf("deleting import for service %+v, as it no longer matches an import filter", service.ServiceKey)
 			return c.deleteService(service, existing), nil
@@ -758,6 +772,8 @@ func (c *Controller) updateService(service *federationmodel.ServiceMessage, exis
 		Name:      string(localService.Hostname),
 		Namespace: localService.Attributes.Namespace,
 	}] = struct{}{}
+
+	c.statusHandler.ImportUpdated(*importedName, service.Hostname)
 
 	return updatedConfigs, nil
 }
@@ -834,6 +850,7 @@ func (c *Controller) removeServiceFromStore(service federationmodel.ServiceKey) 
 }
 
 func (c *Controller) watch(eventCh chan *federationmodel.WatchEvent, stopCh <-chan struct{}) error {
+	c.statusHandler.WatchInitiated()
 	url := c.discoveryURL + "/watch"
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
@@ -842,6 +859,15 @@ func (c *Controller) watch(eventCh chan *federationmodel.WatchEvent, stopCh <-ch
 	}
 	req.Header.Add("discovery-address", c.networkAddress)
 	resp, err := http.DefaultClient.Do(req)
+	defer func() {
+		status := ""
+		if resp != nil {
+			status = resp.Status
+		} else if err != nil {
+			status = err.Error()
+		}
+		c.statusHandler.WatchTerminated(status)
+	}()
 	if err != nil {
 		return err
 	}
@@ -849,10 +875,13 @@ func (c *Controller) watch(eventCh chan *federationmodel.WatchEvent, stopCh <-ch
 		return fmt.Errorf("status code is not OK: %v (%s)", resp.StatusCode, resp.Status)
 	}
 
+	c.statusHandler.Watching()
+
 	// connection was established successfully. reset backoffPolicy
 	c.backoffPolicy.Reset()
 
 	dec := json.NewDecoder(resp.Body)
+	defer resp.Body.Close()
 	for {
 		select {
 		case <-stopCh:
@@ -876,6 +905,7 @@ func (c *Controller) resync() uint64 {
 	c.updateGateways(svcList)
 	if svcList != nil {
 		c.convertServices(svcList)
+		c.statusHandler.FullSyncComplete()
 		return svcList.Checksum
 	}
 	return 0

pkg/servicemesh/federation/server/server.go
@@ -34,6 +34,7 @@ import (
 	"istio.io/istio/pkg/config/host"
 	"istio.io/istio/pkg/servicemesh/federation/common"
 	federationmodel "istio.io/istio/pkg/servicemesh/federation/model"
+	"istio.io/istio/pkg/servicemesh/federation/status"
 	"istio.io/pkg/log"
 )
 
@@ -49,7 +50,7 @@ type Options struct {
 }
 
 type FederationManager interface {
-	AddMeshFederation(mesh *v1alpha1.MeshFederation, exports *v1alpha1.ServiceExports) error
+	AddMeshFederation(mesh *v1alpha1.MeshFederation, exports *v1alpha1.ServiceExports, statusHandler status.Handler) error
 	DeleteMeshFederation(name string)
 	UpdateExportsForMesh(exports *v1alpha1.ServiceExports) error
 	DeleteExportsForMesh(name string)
@@ -120,7 +121,7 @@ func (s *Server) ingressServiceName(mesh *v1alpha1.MeshFederation) string {
 	return fmt.Sprintf("%s.%s.svc.%s", mesh.Spec.Gateways.Ingress.Name, mesh.Namespace, s.env.DomainSuffix)
 }
 
-func (s *Server) AddMeshFederation(mesh *v1alpha1.MeshFederation, exports *v1alpha1.ServiceExports) error {
+func (s *Server) AddMeshFederation(mesh *v1alpha1.MeshFederation, exports *v1alpha1.ServiceExports, statusHandler status.Handler) error {
 	exportConfig := common.NewServiceExporter(exports, s.defaultExportConfig, exportDomainSuffix(mesh.Name))
 
 	untypedMeshServer, ok := s.meshes.Load(mesh.Name)
@@ -133,6 +134,7 @@ func (s *Server) AddMeshFederation(mesh *v1alpha1.MeshFederation, exports *v1alp
 		env:                      s.env,
 		mesh:                     mesh,
 		exportConfig:             exportConfig,
+		statusHandler:            statusHandler,
 		configStore:              s.configStore,
 		ingressService:           s.ingressServiceName(mesh),
 		currentServices:          make(map[federationmodel.ServiceKey]*federationmodel.ServiceMessage),
@@ -205,6 +207,8 @@ func (s *Server) handleServiceList(response http.ResponseWriter, request *http.R
 		response.WriteHeader(500)
 		return
 	}
+	connection := getClientConnectionKey(request)
+	mesh.statusHandler.FullSyncSent(connection)
 }
 
 func (s *Server) handleWatch(response http.ResponseWriter, request *http.Request) {
@@ -214,7 +218,7 @@ func (s *Server) handleWatch(response http.ResponseWriter, request *http.Request
 		response.WriteHeader(400)
 		return
 	}
-	mesh.handleWatch(response)
+	mesh.handleWatch(response, request)
 }
 
 func (s *Server) Run(stopCh <-chan struct{}) {
@@ -316,7 +320,8 @@ type meshServer struct {
 	mesh         *v1alpha1.MeshFederation
 	exportConfig *common.ServiceExporter
 
-	configStore model.ConfigStoreController
+	statusHandler status.Handler
+	configStore   model.ConfigStoreController
 
 	ingressService  string
 	gatewaySAs      []string
@@ -401,12 +406,23 @@ func (s *meshServer) handleServiceList(response http.ResponseWriter) {
 	}
 }
 
-func (s *meshServer) handleWatch(response http.ResponseWriter) {
+func getClientConnectionKey(request *http.Request) string {
+	forwardedIPs := strings.Split(request.Header.Get("X-Forwarded-For"), ",")
+	if len(forwardedIPs) > 0 {
+		return strings.TrimSpace(forwardedIPs[0])
+	}
+	return request.RemoteAddr
+}
+
+func (s *meshServer) handleWatch(response http.ResponseWriter, request *http.Request) {
 	watch := make(chan *federationmodel.WatchEvent)
 	s.watchMut.Lock()
 	s.currentWatches = append(s.currentWatches, watch)
 	s.watchMut.Unlock()
+	connection := getClientConnectionKey(request)
+	s.statusHandler.RemoteWatchAccepted(connection)
 	defer func() {
+		s.statusHandler.RemoteWatchTerminated(connection)
 		s.watchMut.Lock()
 		for i, w := range s.currentWatches {
 			if w == watch {
@@ -426,7 +442,12 @@ func (s *meshServer) handleWatch(response http.ResponseWriter) {
 	}
 	flusher.Flush()
 	for {
-		event := <-watch
+		var event *federationmodel.WatchEvent
+		select {
+		case event = <-watch:
+		case <-request.Context().Done():
+			return
+		}
 		respBytes, err := json.Marshal(event)
 		if err != nil {
 			s.logger.Errorf("error marshaling watch event: %s", err)
@@ -443,6 +464,7 @@ func (s *meshServer) handleWatch(response http.ResponseWriter) {
 			return
 		}
 		flusher.Flush()
+		s.statusHandler.WatchEventSent(connection)
 	}
 }
 
@@ -485,6 +507,9 @@ func (s *meshServer) resync() {
 			s.logger.Debugf("exporting service %+v as %+v", svcKey, svcMessage.ServiceKey)
 			s.addService(svcKey, svcMessage)
 		}
+	}
+	if err := s.statusHandler.Flush(); err != nil {
+		s.logger.Errorf("error updating federation export status for mesh %s: %s", s.mesh.Name, err)
 	}
 }
 
@@ -587,6 +612,7 @@ func (s *meshServer) addService(svc federationmodel.ServiceKey, msg *federationm
 		Action:  federationmodel.ActionAdd,
 		Service: msg,
 	}
+	s.statusHandler.ExportAdded(svc, msg.Hostname)
 	s.pushWatchEvent(e)
 }
 
@@ -598,6 +624,7 @@ func (s *meshServer) updateService(svc federationmodel.ServiceKey, msg *federati
 		Action:  federationmodel.ActionUpdate,
 		Service: msg,
 	}
+	s.statusHandler.ExportUpdated(svc, msg.Hostname)
 	s.pushWatchEvent(e)
 }
 
@@ -612,6 +639,7 @@ func (s *meshServer) deleteService(svc federationmodel.ServiceKey, msg *federati
 		Action:  federationmodel.ActionDelete,
 		Service: msg,
 	}
+	s.statusHandler.ExportRemoved(svc)
 	s.pushWatchEvent(e)
 }
 