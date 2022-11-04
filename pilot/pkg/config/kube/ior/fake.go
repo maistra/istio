f6bed9f40122307138fd1e7d0369b660e6183f48
Yann Liu <yannliu@redhat.com>
2022-11-04 17:16:50 +0800

    Remove unused functions
    Signed-off-by: Yann Liu <yannliu@redhat.com>

--- pilot/pkg/config/kube/ior/fake.go
+++ pilot/pkg/config/kube/ior/fake.go
@@ -255,22 +255,6 @@ var (
 	countCallsLock sync.Mutex
 )
 
-func countCallsReset() {
-	countCallsLock.Lock()
-	defer countCallsLock.Unlock()
-	countCalls = map[string]int{}
-}
-
-func countCallsGet(k string) int {
-	countCallsLock.Lock()
-	defer countCallsLock.Unlock()
-	v, ok := countCalls[k]
-	if !ok {
-		v = 0
-	}
-	return v
-}
-
 func countCallsIncrement(k string) {
 	countCallsLock.Lock()
 	defer countCallsLock.Unlock()
