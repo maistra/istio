bc2dd719f33db68ba5cb91a93072a659d11b1df1
Daniel Grimm <dgrimm@redhat.com>
2022-11-07 09:28:28 +0100

    Introduce GatewayControllerMode

--- pilot/pkg/bootstrap/server.go
+++ pilot/pkg/bootstrap/server.go
@@ -1149,7 +1149,7 @@ func (s *Server) initControllers(args *PilotArgs) error {
 	if err := s.initCertController(args); err != nil {
 		return fmt.Errorf("error initializing certificate controller: %v", err)
 	}
-	if features.EnableEnhancedResourceScoping.Load() {
+	if features.EnableEnhancedResourceScoping.Load() || features.EnableGatewayControllerMode {
 		// setup namespace filter
 		args.RegistryOptions.KubeOptions.DiscoveryNamespacesFilter = s.multiclusterController.DiscoveryNamespacesFilter
 	}
