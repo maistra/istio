c281a49078b9e9acf667ce8b24b03e454711d708
Jacek Ewertowski <jewertow@redhat.com>
2023-03-31 14:04:15 +0200

    Fix and refactor tests of gateway API
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

--- tests/integration/servicemesh/maistra/maistra.go
+++ tests/integration/servicemesh/maistra/maistra.go
@@ -84,6 +84,19 @@ func ApplyServiceMeshCRDs(ctx resource.Context) (err error) {
 	return err
 }
 
+func CleanupGatewayAPICRDs() resource.SetupFn {
+	return func(ctx resource.Context) error {
+		for _, c := range ctx.Clusters() {
+			if err := c.DeleteYAMLFiles(
+				"", filepath.Join(env.IstioSrc, "tests/integration/pilot/testdata/gateway-api-crd.yaml"),
+			); err != nil && !errors.IsNotFound(err) {
+				return err
+			}
+		}
+		return nil
+	}
+}
+
 func ApplyGatewayAPICRDs(version string) resource.SetupFn {
 	return func(ctx resource.Context) error {
 		for _, c := range ctx.Clusters() {
