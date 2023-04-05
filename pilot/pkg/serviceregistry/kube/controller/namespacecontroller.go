cba217eba5c5bef26571ebc7526f3d9724706c4e
Jacek Ewertowski <jewertow@redhat.com>
2023-04-05 21:11:05 +0200

    OSSM-3703: Fix AUTO_RELOAD_PLUGIN_CERTS in multi-tenant mode
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

--- pilot/pkg/serviceregistry/kube/controller/namespacecontroller.go
+++ pilot/pkg/serviceregistry/kube/controller/namespacecontroller.go
@@ -144,9 +144,16 @@ func (nc *NamespaceController) startCaBundleWatcher(stop <-chan struct{}) {
 	for {
 		select {
 		case <-watchCh:
-			namespaceList, _ := nc.namespaceLister.List(labels.Everything())
-			for _, ns := range namespaceList {
-				nc.namespaceChange(ns)
+			var namespaceList []*v1.Namespace
+			if nc.usesMemberRollController {
+				for _, ns := range nc.namespaces.List() {
+					nc.syncNamespaceByName(ns)
+				}
+			} else {
+				namespaceList, _ = nc.namespaceLister.List(labels.Everything())
+				for _, ns := range namespaceList {
+					nc.namespaceChange(ns)
+				}
 			}
 		case <-stop:
 			return
@@ -188,12 +195,13 @@ func (nc *NamespaceController) syncNamespace(ns *v1.Namespace) {
 	if nc.DiscoveryNamespacesFilter != nil && !nc.DiscoveryNamespacesFilter.FilterNamespace(ns.ObjectMeta) {
 		return
 	}
+	nc.queue.Add(types.NamespacedName{Name: ns.Name})
+}
 
-	// If a MemberRoll controller is in use, and the set of
-	// namespaces still includes the one for this ConfigMap,
-	// then recreate the ConfigMap, otherwise do nothing.
-	if nc.usesMemberRollController && !nc.namespaces.Contains(ns.Name) {
+func (nc *NamespaceController) syncNamespaceByName(ns string) {
+	// skip special kubernetes system namespaces
+	if inject.IgnoredNamespaces.Contains(ns) {
 		return
 	}
-	nc.queue.Add(types.NamespacedName{Name: ns.Name})
+	nc.queue.Add(types.NamespacedName{Name: ns})
 }
