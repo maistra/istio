f1fd702f23b372e17683fc88acc78d15978a1c62
Yann Liu <yannliu@redhat.com>
2023-11-01 21:19:27 +0800

    Set UID/GID in gateway deployments
    Partially pick changes from the upstream commits below.
    
    * Remove the need of anyuid permission on OpenShift (#45394)
    
    - Remove the hard-coded usages of `runAsUser` and `runAsGroup` in charts
    - Where necessary, replace them with a helm field `.ProxyUID` and
      `.ProxyGID`.
    - When running in OpenShift, these fields will be set at runtime based
      on annotations present on the pod's namespace.
    - When not on OpenShift (strictly speaking, when such annotations are
      not present), these values fallback to the current, defauls
      value of 1337.
    
    With this change we can remove the extra steps related to `anyuid` in the [OpenShift setup
    page](https://istio.io/v1.17/docs/setup/platform-setup/openshift/).
    
    * Fix proxy UID/GID in Gateway templates (#46898)
    
    Co-authored-by: Yann Liu <yannliu@redhat.com>
    Co-authored-by: Jonh Wendell <jonh.wendell@redhat.com>
    Signed-off-by: Yann Liu <yannliu@redhat.com>

--- pilot/pkg/config/kube/gateway/deploymentcontroller.go
+++ pilot/pkg/config/kube/gateway/deploymentcontroller.go
@@ -298,6 +298,12 @@ func (d *DeploymentController) configureIstioGateway(log *istiolog.Scope, gw gat
 	}
 	log.Info("reconciling")
 
+	var ns *corev1.Namespace
+	if d.namespaces != nil {
+		ns = d.namespaces.Get(gw.Namespace, "")
+	}
+	proxyUID, proxyGID := GetProxyIDs(ns)
+
 	defaultName := getDefaultName(gw.Name, &gw.Spec)
 	input := TemplateInput{
 		Gateway:        &gw,
@@ -307,6 +313,8 @@ func (d *DeploymentController) configureIstioGateway(log *istiolog.Scope, gw gat
 		ClusterID:      d.clusterID.String(),
 		KubeVersion122: kube.IsAtLeastVersion(d.client, 22),
 		Revision:       d.revision,
+		ProxyUID:       proxyUID,
+		ProxyGID:       proxyGID,
 	}
 
 	if overwriteControllerVersion {
@@ -500,6 +508,8 @@ type TemplateInput struct {
 	ClusterID      string
 	KubeVersion122 bool
 	Revision       string
+	ProxyUID       int64
+	ProxyGID       int64
 }
 
 func extractServicePorts(gw gateway.Gateway) []corev1.ServicePort {
