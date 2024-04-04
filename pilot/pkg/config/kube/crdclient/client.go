4a6e014511a2dbc15a00fa39989f4d3f5283683a
Marko Lukša <marko.luksa@gmail.com>
2024-04-04 21:34:43 +0200

    OSSM-6177 Prevent nil pointer dereference when analysis is enabled and GatewayAPI is disabled

--- pilot/pkg/config/kube/crdclient/client.go
+++ pilot/pkg/config/kube/crdclient/client.go
@@ -430,7 +430,10 @@ func handleCRDAdd(cl *Client, name string) {
 	resourceGVK := s.GroupVersionKind()
 	gvr := s.GroupVersionResource()
 
-	if cl.client.IsMultiTenant() && resourceGVK == gvk.GatewayClass {
+	if !features.EnableGatewayAPI && s.Group() == gvk.KubernetesGateway.Group {
+		scope.Infof("Skipping CRD %v as GatewayAPI support is not enabled", s.GroupVersionKind())
+		return
+	} else if cl.client.IsMultiTenant() && resourceGVK == gvk.GatewayClass {
 		scope.Infof("Skipping CRD %v as it is not compatible with maistra multi-tenancy", s.GroupVersionKind())
 		return
 	}
